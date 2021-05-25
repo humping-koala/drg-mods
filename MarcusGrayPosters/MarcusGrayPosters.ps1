ee5a295912392e95b3d8063cf5daba780a17f42b
Humping Koala <humping.koala@gmail.com>
2021-05-25 16:36:49 +0300

    Updated MarcusGray Posters to 1.1

--- MarcusGrayPosters/MarcusGrayPosters.ps1
+++ MarcusGrayPosters/MarcusGrayPosters.ps1
@@ -2,11 +2,12 @@ Set-Location $PSScriptRoot # Change working directory to script location
 
 ### Project-specific variables ###
 
-$ProjectVersion = "1.0"
+$ProjectVersion = "1.1"
 $ProjectName = "MarcusGray Posters" # Name of your Unreal Engine 4 project
 $ProjectDirectory = "." # Path to your Unreal Engine 4 project / Content folder. Current directory by default.
 $IsUE4Project = $true # If set to $true, Unreal Automation Tool will be used to cook the project first and pak the resulting files
-$FilesToPak = @('PropogandaPoster01', 'PropogandaPoster02', 'PropogandaPoster03') # List of files to include in mod pak
+$FilesToPak = @('PropogandaPoster01', 'PropogandaPoster02', 'PropogandaPoster03', 
+                'OpenAlpha_Poster_300ppi_Without_Text_3508x4961px') # List of files to include in mod pak
 
 
 ### Import and execute the pak script ###
