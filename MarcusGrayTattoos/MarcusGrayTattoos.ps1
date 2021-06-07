f470ef51a89127f2ba7e57c4afbd21a17114369d
Humping Koala <humping.koala@gmail.com>
2021-06-07 19:16:38 +0300

    [MarcusGray Tattoos] Reduced cooked texture size to optimized size

--- MarcusGrayTattoos/MarcusGrayTattoos.ps1
+++ MarcusGrayTattoos/MarcusGrayTattoos.ps1
@@ -6,7 +6,7 @@ $ProjectVersion = "1.0"
 $ProjectName = "MarcusGray Tatoos" # Name of your mod
 $ProjectDirectory = "." # Path to your Unreal Engine 4 project / Content folder. Current directory by default.
 $IsUE4Project = $true # If set to $true, Unreal Automation Tool will be used to cook the project first and pak the resulting files
-$SkipCooking = $true # Set to $true to prevent waste of time on cooking the project if we made binary edits only.
+$SkipCooking = $false # Set to $true to prevent waste of time on cooking the project if we made binary edits only.
 $LaunchGame = $true # Launches the game from steam after paking is completed
 $FilesToPak = @('T_Tattoo_AllForMolly', 'T_Tattoo_NeverFeltBetter_Green',
                 'T_Tattoo_Sleeve_BoscoForever_Color_V4', 'T_Tattoo_Sleeve_RockAndThunder_Color_V10', 
